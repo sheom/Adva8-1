fb9fdc32dd9522d4732c37c52f7f9a383a86a3c1
sheom <sheo.tiwary@gmail.com>
2024-07-27 23:57:38 +0530

    firebase file upload in Lab onboarding

--- src/Pages/Labs-onboarding/Labs-onboarding.jsx
+++ src/Pages/Labs-onboarding/Labs-onboarding.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 import "./Labs-onboarding.css";
+//
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../../firebase";
+//
 
 function LabsOnboarding() {
   const [labDetails, setLabDetails] = useState({
@@ -11,6 +20,7 @@ function LabsOnboarding() {
     operatingDays: [],
     availableTimeSlots: [],
     labImages: null,
+    imageUrls: [],
   });
 
   const [days, setDays] = useState({
@@ -55,6 +65,81 @@ function LabsOnboarding() {
     features: [],
     availableTimeSlots: [],
   });
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  const [files, setFiles] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length + labDetails.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+
+          // setFormData({
+          //   ...formData,
+          //   imageUrls: labDetails.imageUrls.concat(urls),
+          // });
+          setLabDetails({ 
+            ...labDetails, 
+            imageUrls: labDetails.imageUrls.concat(urls),
+            //labImages: e.target.files[0] 
+          });
+
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setImageUploadError('Image upload failed (2 mb max per image)');
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError('You can only upload 6 images per listing');
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (index) => {
+    setLabDetails({ 
+      ...labDetails, 
+      imageUrls: labDetails.imageUrls.filter((_, i) => i !== index),
+    });
+
+  };
+  
+  /////////////////////////////////////////////////////////////////////////////////////////////
 
   const handleInputChange = (e, setState, state) => {
     const { name, value } = e.target;
@@ -127,14 +212,15 @@ function LabsOnboarding() {
     };
 
     console.log("Payload:", payload);
-
-    // Replace with your POST request logic
-    fetch("https://your-api-endpoint.com/save-details", {
+    alert("Check Payload At console");
+    console.log("Payload after alert: " + JSON.stringify(payload));
+    //Replace with your POST request logic
+    fetch("/api/lab/create", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(payload),
+      body: JSON.stringify(payload.labDetails),
     })
       .then((response) => response.json())
       .then((data) => {
@@ -237,10 +323,73 @@ function LabsOnboarding() {
             </div>
           </div>
           <div className="form-row">
-            <div className="column">
+            {/* <div className="column">
               <label>Upload Lab Images</label>
               <input type="file" onChange={handleFileChange} />
               <p>Maximum file size: 1MB</p>
+            </div> */}
+            <div className="column">
+              New file upload component
+              <div className="flex flex-col flex-1 gap-4">
+                <p className="font-semibold">
+                  Images(max 6):
+                  <span className="font-normal text-gray-600 ml-2">
+                    The first image will be the cover
+                  </span>
+                </p>
+                <div className="flex gap-4">
+                  <input
+                    onChange={(e) => setFiles(e.target.files)}
+                    className="p-3 border border-gray-300 rounded w-full"
+                    type="file"
+                    id="images"
+                    accept="image/*"
+                    multiple
+                  />
+                  <button
+                    type="button"
+                    disabled={uploading}
+                    onClick={handleImageSubmit}
+                    className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+                  >
+                    {uploading ? "Uploading..." : "Upload"}
+                  </button>
+                </div>
+                <p className="text-red-700 text-sm">
+                  {imageUploadError && imageUploadError}
+                </p>
+                {labDetails.imageUrls.length > 0 &&
+                  labDetails.imageUrls.map((url, index) => (
+                    <div
+                      key={url}
+                      className="flex justify-between p-3 border items-center"
+                    >
+                      <img
+                        src={url}
+                        alt="listing image"
+                        className="w-20 h-20 object-contain rounded-lg"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => handleRemoveImage(index)}
+                        className="p-3 text-red-700 rounded-lg uppercase hover:opacity-75"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  ))}
+
+                {/* <button
+                  disabled={loading || uploading}
+                  className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+                >
+                  {loading ? "Registering..." : "Register Center"}
+                </button>
+                {error && <p className="text-red-700 text-sm">{error}</p>} */}
+
+
+              </div>
+              End New file upload component
             </div>
             <div className="column">
               <label>Available Time Slots</label>
